745654cbbe65431c140304026048fa4b2dd2ad3d
Matthew-Miceli <mattconnormiceli@gmail.com>
2024-12-29 19:02:54 -0400

    Built color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/GameHub Resources/Logo/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import { px } from "framer-motion";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"10px"}>
       <Image src={logo} boxSize="60px"></Image>
-      <Text>Navbar</Text>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
