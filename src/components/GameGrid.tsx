006832f240a699dcd92f938bc87d6af974ac79b1
Matthew-Miceli <mattconnormiceli@gmail.com>
2024-12-29 20:18:36 -0400

    Building Game Card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,6 @@
+import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { error, games } = useGames();
@@ -6,11 +8,15 @@ const GameGrid = () => {
     return <div>Error: {error}</div>;
   }
   return (
-    <ul>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+      padding="10px"
+      spacing={10}
+    >
       {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
+        <GameCard key={game.id} game={game} />
       ))}
-    </ul>
+    </SimpleGrid>
   );
 };
 
